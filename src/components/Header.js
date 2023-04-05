d93d8278c6b492f3f3994ee30b01b6e3e40058c5
Nijyar Ghazi <nijyar360@gmail.com>
2023-04-05 04:30:55 +0300

    added header components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className="porfolio_header">
+      <div className="header_name_container">
+        <h1 className="header_name">Nizhyar Saeed</h1>
+      </div>
+      <ul className="header_items_list">
+        <li className="header_item">Test</li>
+        <li className="header_item">Test</li>
+        <li className="header_item">Test</li>
+      </ul>
+    </header>
+  );
+}
