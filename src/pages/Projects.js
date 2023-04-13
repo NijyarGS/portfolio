dd5b572e15c3041ab354fda19f59e9feac16fe45
Nijyar Ghazi <nijyar360@gmail.com>
2023-04-13 06:04:35 +0300

    fixed the unused variable warrning fixed the empty CSS warning

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,16 @@
 import ProjectComponent from "../components/ProjectComponent";
 
 export default function Projects() {
-  let myProjects = [
-    {
-      name: "OOP Movie",
-      image: "MisingNo",
-      desciption:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, necessitatibus reprehenderit. Unde porro quidem",
-      Porjecturl: "",
-      SiteURL: "",
-    },
-  ];
+  //   let myProjects = [
+  //     {
+  //       name: "OOP Movie",
+  //       image: "MisingNo",
+  //       desciption:
+  //         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, necessitatibus reprehenderit. Unde porro quidem",
+  //       Porjecturl: "",
+  //       SiteURL: "",
+  //     },
+  //   ];
 
   /* be the next */
 
